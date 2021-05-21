030428c11c8efcc107e35fffdcf74ab515a70d96
ilia.shchepin <ilia.shchepin@heathmont.net>
2021-05-21 14:17:38 +0300

    Allow to pass service as event handler opts

--- lib/spandex_ecto/telemetry_adapter.ex
+++ lib/spandex_ecto/telemetry_adapter.ex
@@ -6,12 +6,12 @@ defmodule SpandexEcto.TelemetryAdapter do
   alias SpandexEcto.EctoLogger
 
   #  this is for ecto_sql 3.0.x
-  def handle_event([app_name, repo_name, :query], total_time, log_entry, _config) when is_integer(total_time) do
-    EctoLogger.trace(log_entry, "#{repo_name}_database", String.to_atom("#{app_name}_#{repo_name}"))
+  def handle_event([app_name, repo_name, :query], total_time, log_entry, config) when is_integer(total_time) do
+    EctoLogger.trace(log_entry, "#{repo_name}_database", service(config, app_name, repo_name))
   end
 
   # This is for ecto_sql >= 3.1
-  def handle_event([app_name, repo_name, :query], measurements, metadata, _config) when is_map(measurements) do
+  def handle_event([app_name, repo_name, :query], measurements, metadata, config) when is_map(measurements) do
     log_entry = %{
       query: metadata.query,
       source: metadata.source,
@@ -22,7 +22,7 @@ defmodule SpandexEcto.TelemetryAdapter do
       result: wrap_result(metadata.result)
     }
 
-    EctoLogger.trace(log_entry, "#{repo_name}_database", String.to_atom("#{app_name}_#{repo_name}"))
+    EctoLogger.trace(log_entry, "#{repo_name}_database", service(config, app_name, repo_name))
   end
 
   def handle_event(event_name, measurements, log_entry, config) when is_list(event_name) do
@@ -33,4 +33,8 @@ defmodule SpandexEcto.TelemetryAdapter do
 
   defp wrap_result(result) when is_atom(result), do: {result, "n/a"}
   defp wrap_result(result), do: result
+
+  defp service(config, app_name, repo_name) do
+    config[:service] || String.to_atom("#{app_name}_#{repo_name}")
+  end
 end
