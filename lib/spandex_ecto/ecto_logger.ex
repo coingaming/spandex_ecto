fcbcd30b9d861e5a39ba2515acad6ad0ab8c7e62
Anil Wadghule <anildigital@gmail.com>
2021-05-28 13:18:26 +0530

    Fix tracing execution of parallel preloads in Ecto

--- lib/spandex_ecto/ecto_logger.ex
+++ lib/spandex_ecto/ecto_logger.ex
@@ -5,6 +5,12 @@ defmodule SpandexEcto.EctoLogger do
   query is being run asynchronously (as in the case of parallel preloads).
   """
 
+  alias Spandex.{
+    Span,
+    SpanContext,
+    Trace
+  }
+
   defmodule Error do
     defexception [:message]
   end
@@ -18,75 +24,107 @@ defmodule SpandexEcto.EctoLogger do
     service = config[:service] || :ecto
     truncate = config[:truncate] || 5000
 
-    if tracer.current_trace_id() do
-      now = :os.system_time(:nano_seconds)
+    caller_pid =
+      case Process.get(:"$callers") do
+        [caller_process | _] ->
+          caller_process
 
-      query =
-        log_entry
-        |> string_query()
-        |> String.slice(0, truncate)
+        _ ->
+          self()
+      end
 
-      num_rows = num_rows(log_entry)
+    now = :os.system_time(:nano_seconds)
+    setup(caller_pid, tracer)
 
-      queue_time = get_time(log_entry, :queue_time)
-      query_time = get_time(log_entry, :query_time)
-      decoding_time = get_time(log_entry, :decode_time)
+    query =
+      log_entry
+      |> string_query()
+      |> String.slice(0, truncate)
 
-      start = now - (queue_time + query_time + decoding_time)
+    num_rows = num_rows(log_entry)
 
-      tracer.start_span(
-        "query",
-        start: start,
-        completion_time: now,
+    queue_time = get_time(log_entry, :queue_time)
+    query_time = get_time(log_entry, :query_time)
+    decoding_time = get_time(log_entry, :decode_time)
+
+    start = now - (queue_time + query_time + decoding_time)
+
+    tracer.update_span(
+      start: start,
+      completion_time: now,
+      service: service,
+      resource: query,
+      type: :db,
+      sql_query: [
+        query: query,
+        rows: inspect(num_rows),
+        db: database
+      ],
+      tags: tags(log_entry)
+    )
+
+    report_error(tracer, log_entry)
+
+    if queue_time != 0 do
+      tracer.start_span("queue")
+      tracer.update_span(service: service, start: start, completion_time: start + queue_time)
+      tracer.finish_span()
+    end
+
+    if query_time != 0 do
+      tracer.start_span("run_query")
+
+      tracer.update_span(
         service: service,
-        resource: query,
-        type: :db,
-        sql_query: [
-          query: query,
-          rows: inspect(num_rows),
-          db: database
-        ],
-        tags: tags(log_entry)
+        start: start + queue_time,
+        completion_time: start + queue_time + query_time
       )
-
-      Logger.metadata(trace_id: tracer.current_trace_id(), span_id: tracer.current_span_id())
-
-      report_error(tracer, log_entry)
-
-      if queue_time != 0 do
-        tracer.start_span("queue")
-        tracer.update_span(service: service, start: start, completion_time: start + queue_time)
-        tracer.finish_span()
-      end
-
-      if query_time != 0 do
-        tracer.start_span("run_query")
-
-        tracer.update_span(
-          service: service,
-          start: start + queue_time,
-          completion_time: start + queue_time + query_time
-        )
-
-        tracer.finish_span()
-      end
-
-      if decoding_time != 0 do
-        tracer.start_span("decode")
-
-        tracer.update_span(
-          service: service,
-          start: start + queue_time + query_time,
-          completion_time: now
-        )
-
-        tracer.finish_span()
-      end
 
       tracer.finish_span()
     end
 
+    if decoding_time != 0 do
+      tracer.start_span("decode")
+
+      tracer.update_span(
+        service: service,
+        start: start + queue_time + query_time,
+        completion_time: now
+      )
+
+      tracer.finish_span()
+    end
+
+    finish_ecto_trace(caller_pid, tracer)
+
     log_entry
+  end
+
+  defp finish_ecto_trace(caller_pid, tracer) do
+    if caller_pid != self() do
+      tracer.finish_trace()
+    else
+      tracer.finish_span()
+    end
+  end
+
+  defp setup(caller_pid, tracer) when is_pid(caller_pid) do
+    if caller_pid == self() do
+      tracer.start_span("query")
+    else
+      case Process.info(caller_pid, :dictionary)[:spandex_trace] do
+        nil ->
+          tracer.start_trace("query")
+
+        %Trace{id: trace_id, stack: [%Span{id: span_id} | _]} ->
+          tracer.continue_trace("query", %SpanContext{trace_id: trace_id, parent_id: span_id})
+
+        %Trace{id: trace_id, stack: []} ->
+          tracer.continue_trace("query", %SpanContext{trace_id: trace_id})
+      end
+    end
+
+    Logger.metadata(trace_id: tracer.current_trace_id(), span_id: tracer.current_span_id())
   end
 
   defp report_error(_tracer, %{result: {:ok, _}}), do: :ok
